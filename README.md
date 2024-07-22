541949a318e3e2cdba9588ede53ea6ef6dbffb1f
Jaewon Lee <smart292929@naver.com>
2024-07-22 21:09:21 +0900

    Setting

---
+++ README.md
@@ -0,0 +1,10 @@
+# 2024-MATLAB-AI-contest
+It is a repository for MATLAB College Student AI Competition
+
+It is sentence to practice.
+# title
+## Subtitle
+### subsubtitle
+
+Text for test  
+test
---
+++ code/detect_infomation_photo.m
@@ -0,0 +1,34 @@
+function weather_info = detect_information_photo(photo_dir,dataset_dir)
+    % Input directory of clothes
+    dir_path = fullfile(photo_dir);
+    clothes_list = dir(dir_path);
+    clothes_list = clothes_list(~ismember({clothes_list.name}, {'.', '..'}));
+    
+    % Input directory of weather database
+    weather_path = fullfile(dataset_dir);
+    database = readtable(weather_path, 'VariableNamingRule', 'preserve');
+    
+    % Preprocessing datetime data
+    database{:,'일시'} = datetime(database{:,'일시'}, 'InputFormat', 'yyyy-MM-dd');
+    
+    weather_info = [];
+    
+    for k = 1: length(clothes_list)
+        file_name = clothes_list(k).name;
+        info = string(split(file_name, '-'));
+        pre_date = info(1) + '-' + info(2) + '-' + info(3);
+        date = datetime(pre_date, 'InputFormat', 'yyyy-MM-dd');
+        pre_region = split(info(4), '.');
+        region = pre_region(1);
+        data = database(database{:,'일시'} == date & database{:,'지점명'} == region,:);
+        weather_info = [weather_info; data];
+    end
+    
+    % Enter only the need data 
+    cols_to_remove = {'지점','지점명','일시','일 최심신적설(cm)','일 최심적설(cm)','안개 계속시간(hr)'};
+    weather_info = removevars(weather_info, cols_to_remove);
+    
+    % Preprocessing NaN data
+    weather_info = fillmissing(weather_info, 'constant', 0);
+end
+
